a4c28c05b83c0cc9a9d8dd967efdefd8cbcb1883
kkukileon305 <kkukileon305@gmail.com>
2024-02-06 14:33:29 +0900

    [FEAT]: 영상, 카테고리 목록, 검색 불러오는 API 기능 추가

---
+++ app/src/main/java/com/goodness/codetadak/api/responses/SearchResponse.kt
@@ -0,0 +1,52 @@
+package com.goodness.codetadak.api.responses
+
+data class SearchResponse(
+	val kind: String,
+	val etag: String,
+	val nextPageToken: String,
+	val regionCode: String,
+	val pageInfo: SearchPageInfo,
+	val items: List<SearchItem>
+)
+
+data class SearchItem(
+	val kind: String,
+	val etag: String,
+	val id: ID,
+	val snippet: Snippet
+)
+
+data class ID(
+	val kind: String,
+	val videoID: String? = null,
+	val channelID: String? = null
+)
+
+data class Snippet(
+	val publishedAt: String,
+	val channelID: String,
+	val title: String,
+	val description: String,
+	val thumbnails: SearchThumbnails,
+	val channelTitle: String,
+	val liveBroadcastContent: String,
+	val publishTime: String
+)
+
+data class SearchThumbnails(
+	val default: SearchDefault,
+	val medium: SearchDefault,
+	val high: SearchDefault
+)
+
+data class SearchDefault(
+	val url: String,
+	val width: Long? = null,
+	val height: Long? = null
+)
+
+data class SearchPageInfo(
+	val totalResults: Long,
+	val resultsPerPage: Long
+)
+

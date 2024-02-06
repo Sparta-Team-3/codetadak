c5518552b57171fd6e6d8bb04ba8831a1f7ee4e7
CodeNewbieee <choi2232@hanmail.net>
2024-02-06 10:56:43 +0900

    푸시전 commit

--- app/src/main/java/com/goodness/codetadak/fragments/MyVideoFragment.kt
+++ app/src/main/java/com/goodness/codetadak/fragments/MyVideoFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.goodness.codetadak.R
 import com.goodness.codetadak.databinding.FragmentMyVideoBinding
 
 class MyVideoFragment : Fragment() {

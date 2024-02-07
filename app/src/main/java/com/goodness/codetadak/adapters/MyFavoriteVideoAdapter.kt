dc42a818baa7c03f7b20c19fc32fcd5bacf2db2b
CodeNewbieee <choi2232@hanmail.net>
2024-02-07 20:14:09 +0900

    git pull 받기전 커밋

---
+++ app/src/main/java/com/goodness/codetadak/adapters/MyFavoriteVideoAdapter.kt
@@ -0,0 +1,25 @@
+package com.goodness.codetadak.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.goodness.codetadak.databinding.MyfavoritevideoListBinding
+
+class MyFavoriteVideoAdapter() : RecyclerView.Adapter<MyFavoriteVideoAdapter.MyVideoHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyFavoriteVideoAdapter.MyVideoHolder {
+        return
+    }
+
+    override fun onBindViewHolder(holder: MyFavoriteVideoAdapter.MyVideoHolder, position: Int) {
+        holder.bind()
+    }
+
+    override fun getItemCount(): Int {
+        return
+    }
+
+    inner class MyVideoHolder(private val binding : MyfavoritevideoListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item:) {
+
+        }
+    }
+}

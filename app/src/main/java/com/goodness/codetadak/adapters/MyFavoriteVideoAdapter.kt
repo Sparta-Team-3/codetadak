f041de92200c26f9cbb627c4b86e0c03d90847eb
CodeNewbieee <choi2232@hanmail.net>
2024-02-07 22:08:34 +0900

    [FEAT] : 마이 비디오 어댑터 생성, 초기 프래그먼트 생성시 어댑터에 리스트 전달하는 함수 구현

--- app/src/main/java/com/goodness/codetadak/adapters/MyFavoriteVideoAdapter.kt
+++ app/src/main/java/com/goodness/codetadak/adapters/MyFavoriteVideoAdapter.kt
@@ -1,26 +1,46 @@
 package com.goodness.codetadak.adapters
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.goodness.codetadak.api.responses.VideoItem
 import com.goodness.codetadak.databinding.MyfavoritevideoListBinding
 
-//class MyFavoriteVideoAdapter() : RecyclerView.Adapter<MyFavoriteVideoAdapter.MyVideoHolder>() {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyFavoriteVideoAdapter.MyVideoHolder {
-//        return
-//    }
-//
-//    override fun onBindViewHolder(holder: MyFavoriteVideoAdapter.MyVideoHolder, position: Int) {
-//        holder.bind()
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return
-//    }
-//
-//    inner class MyVideoHolder(private val binding : MyfavoritevideoListBinding) : RecyclerView.ViewHolder(binding.root) {
-//        fun bind(item:) {
-//
-//        }
-//    }
-//}
+class MyFavoriteVideoAdapter() : RecyclerView.Adapter<MyFavoriteVideoAdapter.MyVideoHolder>() {
+    private var myVideoList = mutableListOf<VideoItem>()
+    interface MyVideoItemClick {
+        fun onClick(view: View, position: Int)
+    }
+    var myVideoItemClick: MyVideoItemClick? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyFavoriteVideoAdapter.MyVideoHolder {
+        return MyVideoHolder(MyfavoritevideoListBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: MyFavoriteVideoAdapter.MyVideoHolder, position: Int) {
+        holder.bind(myVideoList[position])
+        holder.itemView.setOnClickListener {
+            myVideoItemClick?.onClick(it,position)
+        }
+    }
+
+    override fun getItemCount(): Int = myVideoList.size
+
+    inner class MyVideoHolder(private val binding : MyfavoritevideoListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: VideoItem) {
+            with(binding) {
+                Glide.with(root).load(item.snippet.thumbnails.high.url).into(ivMyfavoriteThumbnail)
+                tvMyfavoriteTitle.setText(item.snippet.title)
+                tvMyfavoriteInfo.setText(item.snippet.description)
+
+            }
+        }
+    }
+
+    fun setData(newData : MutableList<VideoItem>) {
+        myVideoList = newData
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/goodness/codetadak/fragments/MyVideoFragment.kt
+++ app/src/main/java/com/goodness/codetadak/fragments/MyVideoFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.goodness.codetadak.R
+import com.goodness.codetadak.adapters.MyFavoriteVideoAdapter
 import com.goodness.codetadak.sharedpreferences.UserInfo
 import com.goodness.codetadak.edtitprofiledialog.EditMyProfileDialog
 import com.goodness.codetadak.edtitprofiledialog.OkClick
@@ -17,6 +19,7 @@ import com.goodness.codetadak.sharedpreferences.App
 class MyVideoFragment : Fragment() {
 	private var _binding: FragmentMyVideoBinding? = null
 	private val binding get() = _binding!!
+	private val myFavoriteVideoAdapter by lazy { MyFavoriteVideoAdapter() }
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 	}
@@ -31,6 +34,8 @@ class MyVideoFragment : Fragment() {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+
+		initList()
 
 		with(binding) {
 			ivMyvideoProfilechange.setOnClickListener {
@@ -52,6 +57,21 @@ class MyVideoFragment : Fragment() {
 		}
 	}
 
+	private fun initList() {
+		with(binding) {
+			rvMyvideo.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL,false)
+			rvMyvideo.setHasFixedSize(true)
+			rvMyvideo.adapter = myFavoriteVideoAdapter.apply {
+				myVideoItemClick = object : MyFavoriteVideoAdapter.MyVideoItemClick{
+					override fun onClick(view: View, position: Int) {
+						// 아이템 목록 클릭시 DetailFragment 호출 및 데이터 전달?
+					}
+				}
+			}
+//			myFavoriteVideoAdapter.setData() // 저장된 값 불러서 리스트 넣기
+		}
+	}
+
 	override fun onResume() {
 		super.onResume()
 		val userInfo = App.prefs.loadUserProfile() // 프로필 불러오기
@@ -70,4 +90,5 @@ class MyVideoFragment : Fragment() {
 		super.onDestroyView()
 		_binding = null
 	}
+
 }

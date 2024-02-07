b58d64efd79586a4afce12fcd89fa1eff2b0acc9
ChooJeongHo <kd64choo@gmail.com>
2024-02-07 18:01:59 +0900

    Feat: 레이아웃 구성 및 더미 데이터로 제대로 구현됐나 확인

---
+++ app/src/main/java/com/goodness/codetadak/adapters/HomeMostViewedAdapter.kt
@@ -0,0 +1,43 @@
+package com.goodness.codetadak.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.goodness.codetadak.api.responses.Item
+import com.goodness.codetadak.api.responses.VideosResponse
+import com.goodness.codetadak.databinding.ItemHomeBinding
+
+class HomeMostViewedAdapter : RecyclerView.Adapter<HomeMostViewedAdapter.ViewHolder>() {
+    private var items = listOf<Item>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeMostViewedAdapter.ViewHolder {
+        val binding = ItemHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeMostViewedAdapter.ViewHolder, position: Int) {
+        val item = items[position]
+        holder.ivThumbnail.setImageResource(item.thumbnail)
+        holder.tvTitle.text = item.title
+        holder.tvDescription.text = item.description
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun getItemId(position: Int): Long {
+        return super.getItemId(position)
+    }
+
+    inner class ViewHolder(private val binding: ItemHomeBinding) : RecyclerView.ViewHolder(binding.root) {
+        val ivThumbnail = binding.ivItemHomeThumbnail
+        val tvTitle = binding.tvItemHomeTitle
+        val tvDescription = binding.tvItemHomeDescription
+    }
+
+    fun setData(newData: List<Item>) {
+        items = newData
+        notifyDataSetChanged()
+    }
+
+}

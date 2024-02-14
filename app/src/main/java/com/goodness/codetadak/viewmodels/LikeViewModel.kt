ef5a079e7f5e0b5603167dd12248bb70e6f6252d
kkukileon305 <kkukileon305@gmail.com>
2024-02-14 14:42:05 +0900

    [FEAT]: SharedPreferences를 이용하여 저장 기능 구현

--- app/src/main/java/com/goodness/codetadak/viewmodels/LikeViewModel.kt
+++ app/src/main/java/com/goodness/codetadak/viewmodels/LikeViewModel.kt
@@ -5,9 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.goodness.codetadak.api.responses.VideoItem
+import com.goodness.codetadak.sharedpreferences.App
 
 class LikeViewModel : ViewModel() {
-	private var _likeVideos: MutableLiveData<List<VideoItem>> = MutableLiveData(emptyList())
+	private var _likeVideos: MutableLiveData<List<VideoItem>> = MutableLiveData(App.prefs.loadMyFavorite())
 
 	val likeVideos: LiveData<List<VideoItem>>
 		get() = _likeVideos
@@ -15,10 +16,13 @@ class LikeViewModel : ViewModel() {
 	fun setLikeList(newVideo: VideoItem) {
 		val isHasVideo = _likeVideos.value?.any { video -> video.id == newVideo.id } ?: false
 
-		_likeVideos.value = if (isHasVideo) {
+		val result = if (isHasVideo) {
 			_likeVideos.value?.filterNot { video -> video.id == newVideo.id }?.sortedBy { it.snippet.title }
 		} else {
 			_likeVideos.value?.plus(newVideo)?.sortedBy { it.snippet.title }
-		}
+		} ?: emptyList()
+
+		_likeVideos.value = result
+		App.prefs.saveMyFavorite(result)
 	}
 }
